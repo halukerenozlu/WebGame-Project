14bbd5a46bdbfd6ca89640d9c1134bdeff2cca77
Haluk Eren Özlü <hlk.eren1907@gmail.com>
2025-05-15 11:32:30 +0300

    ufak değişiklikler ve test

---
+++ public/js/i18n.js
@@ -0,0 +1,31 @@
+// düzenlenicek...
+export const i18n = {
+  tr: {
+    placeholders: {
+      username: "Kullanıcı adı",
+      selectCat: "Kategori seçin",
+      selectLang: "Dil seçin",
+      startButton: "Başla",
+    },
+    categories: {
+      general: "Genel Kültür",
+      science: "Bilim",
+      history: "Tarih",
+      sports: "Spor",
+    },
+  },
+  en: {
+    placeholders: {
+      username: "Username",
+      selectCat: "Select category",
+      selectLang: "Select language",
+      startButton: "Start",
+    },
+    categories: {
+      general: "General Knowledge",
+      science: "Science",
+      history: "History",
+      sports: "Sports",
+    },
+  },
+};
